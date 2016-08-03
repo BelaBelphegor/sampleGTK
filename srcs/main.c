4fc6ac8c4822c1bd35e4b9fe7c526f1d1a6e712e
tiboitel <tiboitel@student.42.fr>
2016-08-03 21:56:16 +0200

    Pls

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/03 17:20:03 by tiboitel          #+#    #+#             */
-/*   Updated: 2016/08/03 18:23:49 by tiboitel         ###   ########.fr       */
+/*   Updated: 2016/08/03 21:55:59 by tiboitel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
-static void	gtk_ondestroy(GtkWidget *pwidget, gpointer pdata)
+static void	gtk_ondestroy(GtkWidget *pwidget, gpointer data)
 {
 	(void)pwidget;
-	(void)pdata;
+	(void)data;
+	gtk_widget_destroy(pwidget);
 	gtk_main_quit();
 	exit(1);
+}
+
+static void gtk_activate_entry(GtkWidget *pwidget, gpointer data)
+{
+	const gchar		*text;
+	
+	text = gtk_entry_get_text(GTK_ENTRY(pwidget));
+	gtk_label_set_text(GTK_LABEL((GtkWidget *)data), text);;
 }
 
 int			main(int argc, char **argv)
@@ -27,6 +36,10 @@ int			main(int argc, char **argv)
 	GtkWidget	*pwindow;
 	GtkWidget	*plabel;
 	GtkWidget	*pquitbtn;
+	GtkWidget	*pgrid;
+	GtkWidget	*pentry;
+	GtkWidget	*pframe;
+	GtkWidget	*pseparator;
 	gchar		*sutf8;
 	/*
 	 ** @Initialisation de GTK+3.18
@@ -40,20 +53,46 @@ int			main(int argc, char **argv)
 	gtk_window_set_title(GTK_WINDOW(pwindow), "Raytracer Interface");
 	gtk_window_set_default_size(GTK_WINDOW(pwindow), 420, 680);
 	/*
+	 ** @Creation de la table.
+	 */
+	pgrid = gtk_grid_new();
+	//gtk_grid_set_row_homogeneous(GTK_GRID(pgrid), TRUE);
+	gtk_grid_set_column_spacing(GTK_GRID(pgrid), 5);
+	gtk_grid_set_row_spacing(GTK_GRID(pgrid), 10);
+	gtk_grid_set_column_homogeneous(GTK_GRID(pgrid), TRUE);
+	gtk_container_add(GTK_CONTAINER(pwindow), pgrid);
+	/*
+	 ** @Frame d'encadrement
+	 */
+	pframe = gtk_frame_new("Gestion des objets");
+	gtk_grid_attach(GTK_GRID(pgrid), pframe, 0, 0, 2, 5);
+	/*
+	 ** @Creation et ajout du separateur a la grille.
+	 */
+	pseparator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+	gtk_grid_attach(GTK_GRID(pgrid), pseparator, 0, 2, 2, 1);
+	/*
 	 ** @Creation et initialisation du label.
 	 */
 	(void)plabel;
 	(void)sutf8;
-/*	sutf8 = g_locale_to_utf8("Liste des primitives", -1, NULL, NULL, NULL);
+	sutf8 = g_locale_to_utf8("Liste des primitives", -1, NULL, NULL, NULL);
 	plabel = gtk_label_new(sutf8);
 	g_free(sutf8);
-	gtk_container_add(GTK_CONTAINER(pwindow), plabel);
-*/	/*
+	gtk_grid_attach(GTK_GRID(pgrid), plabel, 0, 1, 1, 1);
+
+	/*
+	 ** @Creation et initialisation de l'entree texte.
+	 */
+	pentry = gtk_entry_new();
+	g_signal_connect(G_OBJECT(pentry), "activate", G_CALLBACK(gtk_activate_entry), (GtkWidget *)plabel);
+	gtk_grid_attach(GTK_GRID(pgrid), pentry, 1, 1, 1, 1);
+	/*
 	 ** @Creation et initialisation d'un bouton.
 	 */ 
 	pquitbtn = gtk_button_new_with_label("Quitter");
 	g_signal_connect(G_OBJECT(pquitbtn), "clicked", G_CALLBACK(gtk_ondestroy), NULL);
-	gtk_container_add(GTK_CONTAINER(pwindow), pquitbtn);
+	gtk_grid_attach(GTK_GRID(pgrid), pquitbtn, 0, 3, 2, 1);
 	/*
 	 ** @ Definition du callback pour le signal destroy.
 	 */
